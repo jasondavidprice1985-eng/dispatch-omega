0d5b0716b28cb509edc4f7f87e240681af59ae6b
jasondavidprice1985-eng <jasondavidprice1985@gmail.com>
2026-05-06 19:41:45 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'dispatch-omega-v1';
+const SHELL = ['/index.html', '/manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  const url = e.request.url;
+  if (url.includes('cdn') || url.includes('googleapis') || url.includes('jquery')) {
+    e.respondWith(
+      fetch(e.request).then(res => {
+        caches.open(CACHE).then(c => c.put(e.request, res.clone()));
+        return res;
+      }).catch(() => caches.match(e.request))
+    );
+    return;
+  }
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
